39c5686a4a8dd9cc19f7b16712d26c9bd4d062ec
Anmol Sharma <anmolsharma14125@gmail.com>
2026-09-07 10:54:11 +0530

    Initial commit - Tic Tac Toe React

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Board from "./Tic-tac/Board";
+import "./App.css";
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className="heading">Tic-Tac-Toe</h1>
+      <Board/>
+      </div>
+  )
+}
+
+export default App;
